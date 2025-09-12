900ffc45222f535058ba3fd13f7e0b6a74109e11
VISWA-RAJ <patrosaikasiviswaraj@gmail.com>
2025-09-12 08:36:15 +0530

    Update settings.py

--- edunity_project/settings.py
+++ edunity_project/settings.py
@@ -6,6 +6,7 @@ Django settings for edunity_project project.
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
